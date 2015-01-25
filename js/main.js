2199629d0e72f484fe21b1fb56e5021766ecd8e8
sharkdp <davidpeter@web.de>
2015-01-25 14:28:34 +0100

    New style, score system, removed sweetalert

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 /*global $*/
 /*global _*/
 /*global L*/
-/*global swal*/
 
 'use strict';
 
@@ -21,13 +20,14 @@ var Icons = {
 
 var Quizzity = function() {
     this.cities = []; // cities to guess
+    this.markers = []; // map elements
 
     // Set up the map and tiles
     this.map = L.map('map', {
         doubleClickZoom: false
     });
 
-    L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    this.layer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
         id: 'sharkdp.e01ecf2e',
         maxZoom: Quizzity.maxZoom,
         minZoom: Quizzity.minZoom,
@@ -40,8 +40,10 @@ var Quizzity = function() {
 
     // Register events
     this.map.on('click', _.bind(this.userClick, this));
+    $("#start").click(_.bind(this.newGame, this));
 
-    // Top panel
+    // HTML elements
+    this.dialog = $("#dialog");
     this.panel = $("#panel");
 };
 
@@ -49,17 +51,22 @@ Quizzity.minZoom = 2;
 Quizzity.maxZoom = 5;
 Quizzity.mapCenter = L.latLng(50, 10); // usually a bad idea: a german-based worldview
 
-Quizzity.citiesPerGame = 5;
+Quizzity.citiesPerGame = 3;
 
 Quizzity.prototype.currentCity = function() {
     return this.cities[this.pointer];
 };
 
 Quizzity.prototype.showCity = function() {
-    this.panel.html(this.currentCity().fullName);
+    var prefix = '<i class="fa fa-location-arrow"></i> ';
+    this.panel.html(prefix + this.currentCity().fullName);
+
+    this.startTime = new Date().getTime();
 };
 
 Quizzity.prototype.newGame = function() {
+    this.removeMarkers();
+
     // Select random cities and add information
     this.cities = _(this.dbCities)
         .sample(Quizzity.citiesPerGame)
@@ -78,20 +85,46 @@ Quizzity.prototype.newGame = function() {
     this.pointer = 0;
 
     this.showCity();
+
+    this.dialog.hide();
+    this.panel.slideDown(200);
 };
 
 Quizzity.prototype.showPoints = function() {
-    this.removeMarkers();
+    this.panel.slideUp(200);
 
-    // Calculate points
-    // _.pluck(this.cities, 'points').
+    // Show all markers
+    _.each(this.cities, function(city) {
+        this.showMarkers(city, true);
+    }, this);
 
-    swal({
-        title: "10123 points!",
-        type: "success",
-        confirmButtonText: "Try again!",
-        confirmButtonColor: '#8bc963'
-    }, _.bind(this.newGame, this));
+    // TODO: Allow user to zoom in, maybe show a different map with labels
+
+    // Score
+    var score = _.reduce(this.cities, function(sum, city) {
+        return sum + city.points;
+    }, 0);
+
+    // Average, best and worst distance:
+    var avgdist = Math.round(_.reduce(this.cities, function(sum, city) {
+        return sum + city.distance;
+    }, 0) / Quizzity.citiesPerGame);
+
+    var sorted = _.sortBy(this.cities, 'distance');
+    var bestcity = _.first(sorted);
+
+    var text = '<ul class="fa-ul">';
+    text += '<li><i class="fa-li fa fa-area-chart"></i>Average distance: ' + avgdist.toString() + 'km</li>';
+    text += '<li><i class="fa-li fa fa-thumbs-o-up"></i>Best guess:<br>' + bestcity.distance.toString() + 'km (' + bestcity.fullName + ')</li>';
+    text += '</ul>';
+
+    // TODO: highscore?
+
+    $("#dialogTitle").html(score.toString() + " points!");
+    $("#dialogContent").html(text);
+    $("#dialogLabel").html("Try again!");
+
+    this.dialog.fadeIn();
 };
 
 Quizzity.prototype.gameActive = function() {
@@ -106,45 +139,76 @@ Quizzity.prototype.removeMarkers = function() {
     }
 };
 
-Quizzity.prototype.userClick = function(e) {
-    if (!this.gameActive()) {
-        return;
-    }
-
-    var city = this.currentCity();
-    var guess = e.latlng;
-
-    // Distance in kilometers
-    var dist = Math.round(guess.distanceTo(city.position) / 1000);
-
-    // var method = dist < 500 ? alertify.success : alertify.error;
-    // method(dist.toString() + "km");
-
-    this.removeMarkers();
-    this.markers = [
+Quizzity.prototype.showMarkers = function(city, drawLine) {
+    this.markers.push(
         L.marker(city.position, {
             icon: Icons.city,
             clickable: false,
             keyboard: false,
             title: city.fullName
-        }).addTo(this.map),
+        }).addTo(this.map)
+    );
 
-        L.marker(guess, {
+    this.markers.push(
+        L.marker(city.guess, {
             icon: Icons.guess,
             clickable: false,
             keyboard: false
         }).addTo(this.map)
-    ];
+    );
 
-    // TODO
-    // we could use geodesics here:
-    // https://github.com/henrythasler/Leaflet.Geodesic
-    // L.polyline([guess, city.position], {
-    //     color: 'black',
-    //     weight: 3,
-    //     opacity: 0.6,
-    //     clickable: false
-    // }).addTo(this.map);
+    if (drawLine) {
+        this.markers.push(
+            L.polyline([city.guess, city.position], {
+                color: 'black',
+                weight: 3,
+                opacity: 0.6,
+                clickable: false
+            }).addTo(this.map)
+        );
+    }
+};
+
+Quizzity.prototype.userClick = function(e) {
+    if (!this.gameActive()) {
+        return;
+    }
+
+    var time = (new Date().getTime()) - this.startTime;
+
+    var city = this.currentCity();
+    city.guess = e.latlng;
+
+    // Calculate points
+    var points = 0;
+
+    // Distance in kilometers
+    var dist = Math.round(city.guess.distanceTo(city.position) / 1000);
+
+    if (dist < 15) { // Consider this exact
+        points = 2000;
+    } else if (dist < 1500) {
+        points = 1500 - dist;
+    }
+
+    var multiplier = 1;
+    if (time < 1000) {
+        multiplier = 3;
+    } else if (time < 2000) {
+        multiplier = 2;
+    } else if (time < 3000) {
+        multiplier = 1.5;
+    } else if (time < 5000) {
+        multiplier = 1.2;
+    }
+
+    points *= multiplier;
+    points = Math.round(points);
+
+    // Save for stats
+    city.distance = dist;
+    city.points = points;
+    city.time = time;
 
     // Reset map view
     this.map.setView(Quizzity.mapCenter, Quizzity.minZoom, {
@@ -153,9 +217,14 @@ Quizzity.prototype.userClick = function(e) {
 
     this.pointer += 1;
     if (this.gameActive()) {
+        // Show guess and solution on the map
+        this.removeMarkers();
+        this.showMarkers(city, false);
+
+        // Show next city in panel
         this.showCity();
-    }
-    else {
+    } else {
+        // Game over!
         this.showPoints();
     }
 
@@ -173,7 +242,7 @@ $(document).ready(function() {
         $.getJSON("geodata/cities.json", function(cities) {
             quizzity.dbCities = cities;
 
-            quizzity.newGame();
+            // TODO: feedback that json has loaded
         });
     });
 });
