c9bc7284a3f0ee391af7a64994df16513efd526b
sharkdp <davidpeter@web.de>
2015-09-15 22:40:34 +0200

    Add histogram picture

--- statistics/histogram.py
+++ statistics/histogram.py
@@ -5,20 +5,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 conn = sqlite3.connect("../php/db/games.db")
-points = [row[0] for row in conn.execute("SELECT SUM(points) FROM guesses GROUP BY game")]
+scores = [row[0] for row in conn.execute("SELECT SUM(points) FROM guesses GROUP BY game")]
 
-print("Creating histogram for {} games".format(len(points)))
-
-hist, bins = np.histogram(points, bins=50)
+hist, bins = np.histogram(scores, bins=50)
 
 width = 0.7 * (bins[1] - bins[0])
 center = (bins[:-1] + bins[1:]) / 2
 
 plt.bar(center, hist, align="center", width=width)
 plt.ylim([0, 3000])
+plt.title("Score distribution after {} games".format(len(scores)))
 plt.xlabel("Points")
 plt.ylabel("No. of games")
 
+highscore = max(scores)
+plt.annotate("Highscore " + str(highscore), xy=(highscore, 0), xytext=(highscore - 2500, 1000),
+             arrowprops=dict(facecolor='black', shrink=0.02))
+
 plt.gcf().tight_layout()
 
-plt.savefig("histogram.png", dpi=50)
+plt.savefig("histogram.png", dpi=100)
